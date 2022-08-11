ce7fbb551842962af838f0944a7fd540eb62903f
Jeyakuma6 <110627881+Jeyakuma6@users.noreply.github.com>
2022-08-11 14:56:41 +0530

    Update Users.cs

--- LoginAuthEg/Models/Users.cs
+++ LoginAuthEg/Models/Users.cs
@@ -14,7 +14,7 @@ namespace LoginAuthEg.Models
         public int UserID { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
-        public stiring Confpassword{get:set:}
+        
         
     }
 }
